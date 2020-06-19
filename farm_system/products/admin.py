07b26675013850fa2b1510df6c6e3fc6fc19791d
Velda Kiara <veldakarimi@gmail.com>
2020-06-20 01:36:56 +0300

    registering models to admin files of products

--- farm_system/products/admin.py
+++ farm_system/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import Products, Fertilizer, Pesticide, Machinery, Tools
+
+admin.site.register(Products)
+admin.site.register(Fertilizer)
+admin.site.register(Pesticide)
+admin.site.register(Machinery)
+admin.site.register(Tools)
