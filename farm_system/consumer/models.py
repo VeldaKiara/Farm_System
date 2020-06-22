595dd656f3d15b2041296b061e5e637f2ecc3510
Velda Kiara <veldakarimi@gmail.com>
2020-06-22 22:23:26 +0300

    addition of receiver models.py file

--- farm_system/consumer/models.py
+++ farm_system/consumer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+ from accounts.models import CustomUser
+class Consumer(models.Model):
+	user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f'{self.id}'
+		
+		
+		
+		
