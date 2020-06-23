4ae7b2136269f6a33420609fb8330c45f98ac450
Velda Kiara <veldakarimi@gmail.com>
2020-06-23 12:58:56 +0300

    addition of models in farmers app

--- farm_system/farmer/models.py
+++ farm_system/farmer/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from datetime import datetime
+from django.utils import timezone
+from accounts.models import CustomUser
+from consumer.models import Consumer
 
+class Farmer(models.Model):
+	user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+   
+ 
+    
+
+class Selling(models.Model):
+	consumer = models.ForeignKey(Consumer, on_delete=models.CASCADE)
+	farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE)
+	is_approved = models.BooleanField(default=False)
+	date = models.DateField(default=timezone.now)
+   
+    
+       
