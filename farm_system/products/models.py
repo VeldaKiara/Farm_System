bd2ecb5e8260e4cb1380af0706a8d1fa82f94ccb
Velda Kiara <veldakarimi@gmail.com>
2020-07-02 09:54:00 +0300

    changes in models for products app

--- farm_system/products/models.py
+++ farm_system/products/models.py
@@ -8,6 +8,7 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+    
 class Fertilizer(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=400)
