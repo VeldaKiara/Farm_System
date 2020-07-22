9134c5e8ce2c832759eae22429bbc84e5b03482d
Velda Kiara <veldakarimi@gmail.com>
2020-07-22 23:27:26 +0300

    creationof items on payment_items model

--- farm_system/payment_items/models.py
+++ farm_system/payment_items/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from cart.models import Cart,amount
 # Create your models here.
+class Payment_items(models.Model):
+	id = models.IntegerField(primary_key=True)
+	cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+	is_paid=models.BinaryField(default=False)
+
+    
+
+    class Meta:
+        pass
+		
