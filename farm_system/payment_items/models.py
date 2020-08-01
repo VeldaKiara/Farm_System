fb73cc0af2e847164c3fef1c6470c3e21ec8167e
Velda Kiara <veldakarimi@gmail.com>
2020-08-01 15:05:17 +0300

    changes

--- farm_system/payment_items/models.py
+++ farm_system/payment_items/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from cart.models import Cart
+
+
 # Create your models here.
 class Payment_items(models.Model):
-	id = models.IntegerField(primary_key=True)
-	cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
-	is_paid=models.BinaryField(default=False)
-	class Meta:
-		pass
-    	
-        
-		
+    id = models.IntegerField(primary_key=True)
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    is_paid = models.BinaryField(default=False)
+
+    class Meta:
+        pass
