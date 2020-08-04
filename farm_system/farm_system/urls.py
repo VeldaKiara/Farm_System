73f0520168cc760f3c1195cc96d781f65400384f
Velda Kiara <veldakarimi@gmail.com>
2020-08-04 10:55:46 +0300

    changes

--- farm_system/farm_system/urls.py
+++ farm_system/farm_system/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     #path('farm', include('farm.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
-    path('payment_items', include('payment_items.urls')),
+    path('payment_items/', include('payment_items.urls')),
 
 
 ]
