d3d938ffe0e2c231fee9590820b8253e88e60409
Velda Kiara <veldakarimi@gmail.com>
2020-08-13 10:18:11 +0300

    creation of payments app

--- farm_system/payment_items/models.py
+++ farm_system/payment_items/models.py
@@ -6,7 +6,7 @@ from cart.models import Cart
 class Payment_items(models.Model):
     id = models.IntegerField(primary_key=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    amount_to_pay = models.FloatField( )
+    amount_to_pay = models.FloatField( null=True)
     is_paid = models.BinaryField(default=False)
 
     class Meta:
--- farm_system/payment_items/urls.py
+++ farm_system/payment_items/urls.py
@@ -4,5 +4,5 @@ from payment_items.views import calculatePaymentPrice
 
 app_name = 'Payment_items'
 urlpatterns=[
-    path('/johnnybravo',calculatePaymentPrice,name="johnny bravo")
+    path('johnnybravo',calculatePaymentPrice,name="johnny bravo")
 ]
