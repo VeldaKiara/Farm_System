ab266007d2d99e3115beb914e4558c0acb08b249
Velda Kiara <veldakarimi@gmail.com>
2020-08-17 23:10:00 +0300

    changes

--- farm_system/payments/models.py
+++ farm_system/payments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from payment_items.models import Payment_items
 # Create your models here.
+
+class Payments(models.Model):
+    id = models.IntegerField(primary_key=True)
+    payment_items = models.ForeignKey(Payment_items, on_delete=models.CASCADE)
+    paid_amount = models.FloatField(null=False)
+    
+    
+    class Meta:
+        pass
+       
+
+
+
+
